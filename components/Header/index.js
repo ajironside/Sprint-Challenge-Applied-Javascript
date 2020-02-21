27355eb3e5fb4ce6c3f361e65d71780c43084834
Amanda Ironside <ironside.amanda@gmail.com>
2020-02-21 09:26:34 -0700

    header element created

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,43 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerData = [{
+    date: "MARCH 28, 2019",
+    title: "Lambda Times",
+    temp: "98°"
+}]
+
+function Header(date, title, temp) {
+    //define elements
+    const createHeader = document.createElement('div');
+    const headerDate = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const headerTemp = document.createElement('span');
+
+    //set up structure
+    createHeader.appendChild(headerDate)
+    createHeader.appendChild(headerTemp)
+    createHeader.appendChild(headerTitle)
+
+    //add class names
+    createHeader.classList.add('header')
+    headerDate.classList.add('date')
+    headerTemp.classList.add('temp')
+
+    //add content
+    headerTitle.textContent = title
+    headerDate.textContent = date
+    headerTemp.textContent = temp
+
+    //return
+    return createHeader
+
+}
+
+//grab parent element
+const headerContainer = document.querySelector('.header-container');
+
+//iterate function over data
+headerData.map(data => {
+    headerContainer.appendChild(Header(data.title, data.date, data.temp))
+})
