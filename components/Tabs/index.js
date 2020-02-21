8e8ee604a334d32902e57a0a0900f81d1ad7cfb6
Amanda Ironside <ironside.amanda@gmail.com>
2020-02-21 09:42:24 -0700

    Step 2: create axios request and console log data to review

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//create axios request
+const page = document.querySelector('.topics')
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        //console log data to review structure
+        console.log('res', response)
+
+        //Iterate over topics using tab component
+    })
+    .catch(error => {
+        console.log('Error', error)
+        const whoops = document.createElement('p')
+        whoops.textContent = "An error occured while fetching this content"
+        page.appendChild(whoops)
+    })
+
+//create a tab component
+
+
+//add it to the DOM
