0d0625fd3ecdaf062e9f71057e3e3c6fae3d3e65
Amanda Ironside <ironside.amanda@gmail.com>
2020-02-21 11:31:42 -0700

    cardCreator function added

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,52 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+//article components
+const articleEntry = document.querySelector('.cards-container')
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        //console log data to review structure
+        //console.log('res', response)
+        console.log('res.data.articles', response.data.articles)
+
+        //response.data.articles.
+    })
+    .catch()
+
+//card component
+function cardCreator(headline, authorPhoto, authorName) {
+    //define elements
+    const newCard = document.createElement('div')
+    const newHeadline = document.createElement('div')
+    const newAuthor = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const newAuthorPhoto = document.createElement('img')
+    const newAuthorName = document.createElement('span')
+
+    //append elements
+    newCard.appendChild(newHeadline)
+    newCard.appendChild(newAuthor)
+    newAuthor.appendChild(imgContainer)
+    newAuthor.appendChild(newAuthorName)
+    imgContainer.appendChild(newAuthorPhoto)
+
+    //add class names
+    newCard.classList.add('.card')
+    newHeadline.classList.add('.headline')
+    newAuthor.classList.add('.author')
+    imgContainer.classList.add('.img-container')
+
+    //add content
+    newHeadline.textContent = headline
+    newAuthorPhoto.src = authorPhoto
+    newAuthorName.textContent = authorName
+
+    //return
+    return newCard
+}
+
+//create card for each article and add to DOM
